9110aa2558918df19f0627bf8d9a99e5cd6b6d7b
xrexy <rexydoesthings@gmail.com>
2023-08-31 01:01:40 +0300

    scaffold bot

--- bot/index.ts
+++ bot/index.ts
@@ -1,8 +1,16 @@
+import "dotenv/config"
+
 import { Client } from 'oceanic.js';
-const client = new Client({});
+const client = new Client({
+  auth: process.env.DISCORD_BOT_TOKEN,
+});
 
 client.on('ready', () => {
   console.log('Logged in as ' + client.user.tag);
+});
+
+client.on("error", (err) => {
+  console.error("Something Broke!", err);
 });
 
 client.connect();
