04d65599f75e129e476d26368a2a2120d2efde43
xrexy <rexydoesthings@gmail.com>
2023-08-31 12:12:51 +0300

    create basic discord.js bot

--- bot/index.ts
+++ bot/index.ts
@@ -1,16 +1,47 @@
-import "dotenv/config"
+import 'dotenv/config';
 
-import { Client } from 'oceanic.js';
+import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
+
+const { DISCORD_BOT_TOKEN, DISCORD_CLIENT_ID } = process.env;
+
+// const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN!);
 const client = new Client({
-  auth: process.env.DISCORD_BOT_TOKEN,
+  intents: []
 });
 
 client.on('ready', () => {
-  console.log('Logged in as ' + client.user.tag);
+  console.log('Logged in as ' + client.user?.tag);
 });
 
-client.on("error", (err) => {
-  console.error("Something Broke!", err);
+client.on('error', (err) => {
+  console.error('Something Broke!', err);
 });
 
-client.connect();
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+async function start(token: string | undefined) {
+  // try {
+  //   console.log('Started refreshing application (/) commands.');
+
+  //   await rest.put(Routes.applicationCommands(DISCORD_CLIENT_ID!), {
+  //     body: [
+  //       {
+  //         name: 'ping',
+  //         description: 'Replies with Pong!'
+  //       }
+  //     ]
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
+  client.login(token);
+}
+
+start(DISCORD_BOT_TOKEN);
