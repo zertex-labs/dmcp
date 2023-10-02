97211650021181dd8b0c52325156e3f33b9b1ff9
xrexy <rexydoesthings@gmail.com>
2023-10-02 11:34:30 +0300

    delete test routes & add env example
    Co-authored-by: Sayykii <Sayykii@users.noreply.github.com>

--- api/src/routes/withIdNoSchema/[id].ts
+++
@@ -1,6 +0,0 @@
-import { ApiRoute } from '../../lib/types';
-
-export const GET: ApiRoute = (req) => {
-  const id = (req.params as any).id as string;
-  return { id };
-};
--- bot/src/client/index.ts
+++ bot/src/client/index.ts
@@ -1,28 +1,26 @@
-import {
-  Client,
-  ClientOptions,
-  Collection
-} from 'discord.js';
+import { Client, ClientOptions, Collection } from 'discord.js';
 
 import Logger from '../utils/logger';
 import path from 'path';
 import { ClientCommand } from '../types';
 
-type NicknameWTFIsThis = any
 export class UsableClient extends Client {
   public commands: Collection<String, ClientCommand>;
   public logger: Logger;
+
   constructor(o: ClientOptions) {
     super(o);
     this.commands = new Collection();
-    this.logger = new Logger(path.join(__dirname, "..", "Logs.log")) 
-    this.rest.setToken(process.env.DISCORD_BOT_TOKEN!)
+    this.logger = new Logger(path.join(__dirname, '..', 'Logs.log'));
+
+    this.rest.setToken(process.env.DISCORD_BOT_TOKEN!);
 
     this.on('ready', () => {
       console.log('Logged in as ' + this.user?.tag);
     });
-    
+
     this.on('error', (err) => {
+      this.log(err?.message ?? err);
       console.error('Everything Broke!', err);
       process.exit();
     });
@@ -31,5 +29,4 @@ export class UsableClient extends Client {
   log(text: string) {
     this.logger.log(text);
   }
-
 }
