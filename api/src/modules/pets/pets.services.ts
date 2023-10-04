954f5c1da13146274d7ac8fd680bce947b9263cb
xrexy <rexydoesthings@gmail.com>
2023-10-04 13:01:15 +0300

    add user owned endpoint
    Co-authored-by: Sayykii <Sayykii@users.noreply.github.com>

--- api/src/modules/pets/pets.services.ts
+++ api/src/modules/pets/pets.services.ts
@@ -4,3 +4,13 @@ import { db, tables } from '../../lib/drizzle';
 
 export const createPet = (petInfo: InferInsertModel<typeof tables.pets>) =>
   db.insert(tables.pets).values(petInfo).returning();
+
+export const getOwnedPets = (ownerId: string, includeOwner = false) =>
+  db.query.pets.findMany({
+    where: (pets, { eq }) => eq(pets.ownerId, ownerId),
+    with: includeOwner
+      ? {
+          owner: true
+        }
+      : {}
+  });
---
+++ api/src/routes/pets/owned.ts
@@ -0,0 +1,56 @@
+import { ALL_PETS, type PetType } from '../../lib/constants';
+import { getOwnedPets } from '../../modules/pets/pets.services';
+import { getUser } from '../../modules/users/users.services';
+import { ApiRoute } from '../../lib/types';
+import { DatabaseError } from 'pg';
+
+type Body = {
+  userId: string;
+};
+
+const { API_SECRET, DISCORD_BOT_SECRET } = process.env;
+
+export const POST: ApiRoute<true> = {
+  handler: async (req, res) => {
+    const { userId } = req.body as Body;
+
+    const authorization = req.headers.authorization;
+    if (authorization !== API_SECRET) {
+      res.status(401).send('Invalid API key');
+
+      return;
+    }
+
+    try {
+      return res.status(200).send(await getOwnedPets(userId, true));
+    } catch (e) {
+      console.log(e);
+      res.status(500).send({ message: 'Internal error' });
+    }
+  },
+  additional: {
+    schema: {
+      headers: {
+        type: 'object',
+        properties: {
+          authorization: {
+            type: 'string',
+            minLength: 1
+          }
+        },
+        required: ['authorization']
+      },
+      body: {
+        type: 'object',
+        properties: {
+          userId: {
+            type: 'string',
+            minLength: 17,
+            maxLength: 20
+          }
+        },
+        required: ['userId']
+      }
+    }
+  }
+};
