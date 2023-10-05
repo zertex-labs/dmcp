742e06a7843a39053a99b1ad6bc1f53a519d243f
xrexy <rexydoesthings@gmail.com>
2023-10-05 19:36:04 +0300

    add data comment

--- packages/shared/data/index.ts
+++ packages/shared/data/index.ts
@@ -1,3 +1,5 @@
+// !! IMPORTANT !! Don't modify the json schema directly. Use the "syncEnums" script to generate new property keys(syncs data from corresponding ./types file)
+
 import food from './food.json';
 import pets from './pets.json';
 
