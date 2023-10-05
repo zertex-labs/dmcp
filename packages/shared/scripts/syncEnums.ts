f723dbe4c3192b2687a521a05ac33499ed81e5f5
xrexy <rexydoesthings@gmail.com>
2023-10-05 19:32:23 +0300

    sync food items and fix syncEnums script not removing old properties

--- packages/shared/scripts/syncEnums.ts
+++ packages/shared/scripts/syncEnums.ts
@@ -1,4 +1,9 @@
-import { rarities, availableBonuses, availablePets } from '../types';
+import {
+  rarities,
+  availableBonuses,
+  availablePets,
+  availableFoods
+} from '../types';
 
 const sync = (path: string, data: any) =>
   Bun.write(path, JSON.stringify(data, null, 2));
@@ -12,7 +17,7 @@ const sync = (path: string, data: any) =>
   {
     console.log(`[${i}] Syncing bonuses...`);
     (perf = performance.now()), i++;
-    await sync('src/data/enums/bonuses.json', { enum: availableBonuses });
+    await sync('data/enums/bonuses.json', { enum: availableBonuses });
 
     console.log('Synced bonuses in', performance.now() - perf, 'ms\n');
   }
@@ -20,31 +25,63 @@ const sync = (path: string, data: any) =>
   {
     console.log(`[${i}] Syncing rarities...`);
     (perf = performance.now()), i++;
-    await sync('src/data/enums/rarities.json', { enum: rarities });
+    await sync('data/enums/rarities.json', { enum: rarities });
 
     console.log('Synced rarities in', performance.now() - perf, 'ms\n');
   }
 
   {
-    console.log(`[${i}] Syncing pet keys... (${availablePets.join(', ')})`);
+    console.log(
+      `[${i}] Syncing pet property keys... (${availablePets.join(', ')})`
+    );
     (perf = performance.now()), i++;
-    const schemaBefore = await Bun.file('src/data/schemas/pets.json').json();
+    const schemaBefore = await Bun.file('data/schemas/pets.json').json();
 
     const newSchema = {
       ...schemaBefore,
       properties: {
-        ...schemaBefore.properties,
         ...availablePets.reduce((acc, pet) => {
           acc[pet] = { $ref: '#/definitions/pet' };
           return acc;
         }, {})
       },
-      required: [...new Set([...schemaBefore.required, ...availablePets])]
+      required: availablePets
     };
 
-    await sync('src/data/schemas/pets.json', newSchema);
+    await sync('data/schemas/pets.json', newSchema);
 
-    console.log('Synced pet keys in', performance.now() - perf, 'ms\n');
+    console.log(
+      'Synced pet property keys in',
+      performance.now() - perf,
+      'ms\n'
+    );
+  }
+
+  {
+    // sync food properties
+    console.log(`[${i}] Syncing food properties...`);
+    (perf = performance.now()), i++;
+    const schemaBefore = await Bun.file('data/schemas/food.json').json();
+    const foods = availableFoods.map((food) => food.toUpperCase());
+
+    const newSchema = {
+      ...schemaBefore,
+      properties: {
+        ...foods.reduce((acc, food) => {
+          acc[food] = { $ref: '#/definitions/food' };
+          return acc;
+        }, {})
+      },
+      required: foods
+    };
+
+    await sync('data/schemas/food.json', newSchema);
+
+    console.log(
+      'Synced food property keys in',
+      performance.now() - perf,
+      'ms\n'
+    );
   }
 
   console.log(
