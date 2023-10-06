9b0982a71ce12ecf27a7a2393b5fb27aed617e36
xrexy <rexydoesthings@gmail.com>
2023-10-06 19:21:09 +0300

    fix pets schema using stats instead of perks

--- packages/shared/types/perks.ts
+++ packages/shared/types/perks.ts
@@ -1,2 +1,10 @@
+import { PlayerStat } from './stats';
+
 export const availablePerks = ['PERK_SPEED', 'PERK_FARMING'] as const;
 export type AvailablePerk = (typeof availablePerks)[number];
+
+export type PerkType = {
+  baseValue: number;
+  maxValue: number;
+  appliesTo: PlayerStat[];
+};
