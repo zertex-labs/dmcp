7d91fde8e0dd7f407ce412839bf93e8b9fb323e2
xrexy <rexydoesthings@gmail.com>
2023-10-06 19:07:01 +0300

    change shared data comment

--- packages/shared/data/index.ts
+++ packages/shared/data/index.ts
@@ -1,4 +1,4 @@
-// !! IMPORTANT !! Don't modify the json schema directly. Use the "syncEnums" script to generate new property keys(syncs data from corresponding ./types file)
+// !! IMPORTANT !! Don't modify the json schema directly. Use the "syncData" script to generate new property keys(syncs data from corresponding ./types file)
 
 import food from './food.json';
 import pets from './pets.json';
