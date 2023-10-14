c1f9606649c063df5f30ac0b815e3eceb222d695
xrexy <rexydoesthings@gmail.com>
2023-10-14 21:50:17 +0300

    remove useless updaters

--- packages/api/src/redis/index.ts
+++ packages/api/src/redis/index.ts
@@ -1,11 +1,5 @@
 import { Redis } from '@upstash/redis/nodejs'
-import { registerFarmingUpdater, registerLeaderboardUpdater } from '.'
 
 export const redis = Redis.fromEnv()
 
-registerLeaderboardUpdater()
-registerFarmingUpdater()
-
 export * from './getAllItems'
-export * from './leaderboard'
-export * from './users'
