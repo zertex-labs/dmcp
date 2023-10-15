ffaf6d6e1005da42a5b56d88e85896aebda68d33
xrexy <rexydoesthings@gmail.com>
2023-10-15 22:21:40 +0300

    fix the requireApiSecret type

--- packages/api/src/utils/requireApiSecret.ts
+++ packages/api/src/utils/requireApiSecret.ts
@@ -1,7 +1,14 @@
 import { response } from './response'
 
-// designed to be used in Elysia's beforeHandle
-// @ts-expect-error We just don't want to repeat this 100 times and I really can't be bothered to search for the proper type atm
-export function requireApiSecret({ isApiSecretPresent }) {
+/**
+ * @example
+ * .get(
+ *  'example',
+ *  ctx => { ... },
+ *  { beforeHandle: requireApiSecret }
+ * )
+ * @returns If the API secret is not present, returns a 401 response and will not execute the endpoint.
+ */
+export function requireApiSecret({ isApiSecretPresent }: { isApiSecretPresent: () => boolean }) {
   if (!isApiSecretPresent()) return response.predefined.notAuthorized
 }
