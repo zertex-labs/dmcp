a6e0f5eac856582de65a4d58c4d69068a42bc671
xrexy <rexydoesimportantthings@gmail.com>
2023-10-21 12:56:01 +0300

    refactor getAllItems to work with any value or rediskey constructor

--- packages/api/src/redis/getAllItems.ts
+++ packages/api/src/redis/getAllItems.ts
@@ -1,13 +1,23 @@
 import type { PrefixKey } from '.'
-import { PREFIXES, redis } from '.'
+import { PREFIXES, createRedisKey, redis } from '.'
 
+// type Args = <T extends string | PrefixKey>(prefix: T) => T extends string ? [T] : [T, string]
+// type Args<PrefixType extends (string | PrefixKey) & ({})> = PrefixType extends string ? [PrefixType] : [PrefixType, string]
+
+// if prefix is a string args should be just a string
+// if it's PrefixKey there's should a value:string arg as well
 export async function getAllItems<Keys extends PropertyKey, Stored>(
-  prefix: string,
+  o: string | { key: PrefixKey; value: string; suffix?: string },
 ): Promise<Record<Keys, Stored>> {
   let cursor = 0
   const items = {}
 
-  const match = prefix
+  let match: string
+  if (typeof o === 'string')
+    match = o
+  else
+    match = createRedisKey(o.key, o.value, o.suffix)
+
   do {
     const result = await redis.scan(cursor, { match })
 
@@ -20,7 +30,7 @@ export async function getAllItems<Keys extends PropertyKey, Stored>(
         const item = items[i][0]
         const key = keys[i]!
         const itemName = key.slice(match.length - 1)
-        ;(items as any)[itemName] = item
+          ; (items as any)[itemName] = item
       }
     }
   } while (cursor !== 0)
