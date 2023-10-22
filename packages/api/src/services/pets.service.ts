186430af3236de1f9559103fa436d675405b1a78
xrexy <rexydoesimportantthings@gmail.com>
2023-10-22 15:09:44 +0300

    add getPet caching

--- packages/api/src/services/pets.service.ts
+++ packages/api/src/services/pets.service.ts
@@ -1,14 +1,24 @@
-import type { Pet } from 'shared'
 import db from '../db'
+import { createRedisKey, redis } from '../redis'
+import type { PetSelect as Pet } from '../db/schema/pets'
 import type { ServiceResponse } from './types'
 
 export async function getPet(o: { uuid: string; ownerId?: string }): Promise<ServiceResponse<Pet | undefined>> {
   const { uuid, ownerId } = o
 
   try {
-    const pet = await db.query.pets.findFirst({
+    const key = createRedisKey('pet', uuid)
+    let pet = await redis.json.get(key) as Pet | undefined
+
+    if (pet) return { status: 'success', data: pet }
+
+    pet = await db.query.pets.findFirst({
       where: (pets, { eq, and }) => ownerId ? and(eq(pets.ownerId, ownerId), eq(pets.uuid, uuid)) : eq(pets.uuid, uuid),
     })
+
+    if (!pet) return { status: 'error', error: 'Pet not found' }
+
+    redis.json.set(key, '$', pet)
 
     return { status: 'success', data: pet }
   }
